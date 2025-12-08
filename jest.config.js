0010ac45e0dde84bf6d2f818ae55099d7d13f602
edissa <edissa@gamil.com>
2025-12-08 20:47:08 +0200

    Fix jest.config.js - convert from JSON to JavaScript module format

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,24 @@
-{
-  "testEnvironment": "node",
-  "coverageDirectory": "coverage",
-  "collectCoverageFrom": [
-    "index.js",
-    "src/**/*.js",
-    "!**/__tests__/**",
-    "!**/node_modules/**"
+module.exports = {
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'index.js',
+    'src/**/*.js',
+    '!**/__tests__/**',
+    '!**/node_modules/**'
   ],
-  "coverageThreshold": {
-    "global": {
-      "branches": 50,
-      "functions": 50,
-      "lines": 50,
-      "statements": 50
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50
     }
   },
-  "testMatch": [
-    "**/__tests__/**/*.test.js"
+  testMatch: [
+    '**/__tests__/**/*.test.js'
   ],
-  "setupFilesAfterEnv": [],
-  "verbose": true,
-  "testTimeout": 10000
-}
+  setupFilesAfterEnv: [],
+  verbose: true,
+  testTimeout: 10000
+};
