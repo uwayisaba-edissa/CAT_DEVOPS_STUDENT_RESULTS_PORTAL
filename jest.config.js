82623726925b7c6af143d6cafda52bcf6564d30c
edissa <edissa@gamil.com>
2025-12-08 22:12:35 +0200

    Fix Jest configuration syntax error

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,16 @@ module.exports = {
   testEnvironment: 'jsdom',
   collectCoverageFrom: [
     '**/*.{js,jsx}',
-    '!**/node_modules/**'
-  ]
+    '!**/node_modules/**',
+    '!**/dist/**',
+    '!jest.config.js',
+    '!coverage/**'
+  ],
+  testMatch: [
+    '**/__tests__/**/*.test.js',
+    '**/?(*.)+(spec|test).js'
+  ],
+  moduleFileExtensions: ['js', 'json'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html']
 };
